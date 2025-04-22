cc2e4b8641218b5be29b3c7c17fe8ac7bff35991
Umar Farouk <yasmarfaq@yahoo.com>
2025-04-22 23:20:06 +0100

    modified packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,35 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { clean } = require('gh-pages');
 
-
-  module.exports = {
-    mode: 'production',
-    entry: {
-       app: './src/index.js',
-    },
-
-    devtool: 'source-map',
-    devServer: {
-      static: './dist',
-      hot: true,
-    },
-
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        },
-      ],
-    },
-
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Todo-App',
-        template: './src/template.html', 
-      }),
+module.exports = {
+  mode: 'production',
+  entry: {
+    app: './src/index.js',
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    
-    
-    output: {
-      filename: 'app.bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-      clean: true,
-
-    },
-
-  };
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Todo-App',
+      template: './src/template.html',
+    }),
+  ],
+  output: {
+    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    // This is important for GitHub Pages
+    publicPath: './'
+  },
+};
