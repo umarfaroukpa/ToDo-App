442db445872561102a828d2db1a1afa545a73d63
Umar Farouk <yasmarfaq@yahoo.com>
2025-04-22 23:31:42 +0100

    modified branch

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'production',
@@ -15,7 +16,10 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
       },
     ],
   },
@@ -23,13 +27,16 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'Todo-App',
       template: './src/template.html',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
     }),
   ],
   output: {
     filename: 'app.bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    // This is important for GitHub Pages
-    publicPath: './'
+    publicPath: './',
   },
 };
